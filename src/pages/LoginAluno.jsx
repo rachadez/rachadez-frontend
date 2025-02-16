e04c47c85ff7c5e64758aca32ec43ed78d39dd13
LeilaFarias <leilafarias1324@gmail.com>
2025-02-16 19:01:29 -0300

    feat: add componente input email e variações

--- src/pages/LoginAluno.jsx
+++ src/pages/LoginAluno.jsx
@@ -1,4 +1,5 @@
 import './LoginAluno.css';
+import InputEmail from './components/InputEmail';
 import { useNavigate } from 'react-router-dom';
 
 function LoginAluno() {
@@ -14,7 +15,7 @@ function LoginAluno() {
         <div className="login-aluno">
             <h1>Login do Aluno</h1>
             <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Email" />
+                <InputEmail />
                 <input type="password" placeholder="Senha" />
                 <button type="submit">Entrar</button>
             </form>
