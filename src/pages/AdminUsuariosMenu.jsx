1eb3dee537d891a3c7f0bb86c37244dcf2aacb56
Ananda Vilar Vidal <115959226+4nandaw@users.noreply.github.com>
2025-03-23 14:01:26 -0300

    add: new card 'Cadastrar usuário (externo)'

--- src/pages/AdminUsuariosMenu.jsx
+++ src/pages/AdminUsuariosMenu.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header/Header";
 import MainContent from "./components/MainContent/MainContent";
-import { UserRoundPlus, UserRoundPen, UserRoundMinus, UserRoundSearch, UserRoundX } from "lucide-react";
+import { UserRoundPlus, UsersRound, UserRoundPen, UserRoundMinus, UserRoundSearch, UserRoundX } from "lucide-react";
 import Card from "./components/Cards/Card";
 import "./AdminMenu.css";
 
@@ -11,7 +11,8 @@ const AdminUsuarios = () => {
       <MainContent title="Usuários" subtitle="Gerencie todos os usuários cadastrados no sistema! Acesse o tipo de operação nas abas abaixo!" path={"/admin-menu"}/>
 
       <div className="cards-container">
-        <Card icon={UserRoundPlus} title="Cadastrar usuário" path="/cadastrar-usuarios" />
+        <Card icon={UserRoundPlus} title="Cadastrar usuário (UFCG)" path="/cadastrar-usuario-ufcg" />
+        <Card icon={UsersRound} title="Cadastrar usuário (Externo)" path="/cadastrar-usuario-externo" />
         <Card icon={UserRoundPen} title="Editar usuários" path="/editar-usuarios" />
         <Card icon={UserRoundMinus} title="Excluir usuários" path="/excluir-usuarios" />
         <Card icon={UserRoundSearch} title="Visualizar usuários" path="/visualizar-usuarios" />
