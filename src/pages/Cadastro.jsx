e6e9e4222a3fdbc5b8d4928e1ee473f057e8221f
Ananda Vilar Vidal <115959226+4nandaw@users.noreply.github.com>
2025-03-26 13:18:16 -0300

    refact: add components Header + MainContent

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,26 +1,16 @@
 import React from "react";
 import "./Cadastro.css";
-import { ArrowLeft } from "lucide-react";
-import { useNavigate } from "react-router-dom";
 import DefaultButton from "./components/Buttons/DefaultButton";
 import InputTemplate from "./components/InputTemplate/InputTemplate";
+import Header from "./components/Header/Header";
+import MainContent from "./components/MainContent/MainContent";
 
 function Cadastro() {
-  const navigate = useNavigate();
 
   return (
     <div className="cadastro-page">
-      <div className="header">
-        <ArrowLeft className="arrow-left" color="#ffffff" size={40} onClick={() => navigate("/")} />
-        <div className="header-content">
-          <h1>Faça seu cadastro!</h1>
-          <p>
-            Certifique-se de informar seus dados corretamente.
-            <br />
-            Você poderá alterar alguns dos dados quando quiser.
-          </p>
-        </div>
-      </div>
+      <Header />
+      <MainContent path={"/"} title={"Faça seu cadastro!"} subtitle={"Certifique-se de informar seus dados corretamente. Somente membros da comunidade da UFCG (alunos, professores e servidores) podem se cadastrar."}/>
   
       <div className="form-wrapper">
         <div className="form-container">
@@ -31,7 +21,7 @@ function Cadastro() {
             <InputTemplate type="text" label="Matrícula" placeholder="123456789" />
           </div>
           <div className="form-group">
-            <label>Curso</label>
+            <label>Ocupação</label>
             <select>
               <option>Selecione</option>
             </select>
@@ -49,9 +39,6 @@ function Cadastro() {
   
         <div className="button">
           <DefaultButton label={"Realizar cadastro"} />
-          <a href="#" className="link">
-            Não possuo matrícula &gt;
-          </a>
         </div>
       </div>
     </div>
