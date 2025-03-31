3a2c13b4b72079b15071bc8283d8db291a3656e9
kevinicolas22 <kevinicolas2511@gmail.com>
2025-03-30 22:42:35 -0300

    add: integration front + back

--- src/pages/CadastrarReserva.jsx
+++ src/pages/CadastrarReserva.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./CadastrarReserva.css";
 import ConfigIcon from "./components/LogoutIcon/LogoutIcon";
 import TrashIcon from "./components/TrashIcon/TrashIcon";
@@ -10,71 +10,137 @@ import Header from "./components/Header/Header";
 import MainContent from "./components/MainContent/MainContent";
 import ModalTwoOptions from "./components/Modal/ModalTwoOptions";
 import ModalOneOption from "./components/Modal/ModalOneOption";
+import axios from "axios";
 
 const CadastrarReserva = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalType, setModalType] = useState("");
   const [ufcgParticipantInputs, setUfcgParticipantInputs] = useState([]);
   const [externalParticipantInputs, setExternalParticipantInputs] = useState([]);
-
-
-  const handleCadastrarClick = () => {
-    const horarioAgendado = false; // Substitua isso pela lógica para verificar se há reserva no horário
-    const horarioIndisponivel = false; // substitua para a logica de checar se o horario esta disponivel
-    const credenciaisInvalidas = true; /* substitua para a lógica de checar se o local está disponivel,
-                                           checar se o usuario responsavel é permitido, ou 
-                                           qualquer outro problema com os dados inseridos */
-
-    if (horarioAgendado) {
-      setModalType("sobrescrever-horario");
-    } else if (horarioIndisponivel) {
-      setModalType("horario-indisponivel");
-    } else if (credenciaisInvalidas) {
-      setModalType("credenciais-invalidas");
-    } else {
-      setModalType("confirmar-cadastro");
-    }
-
-    setIsModalOpen(true);
-  };
-
-  const handleCadastrarReserva = () => {
-    //add logica de cadastrar reserva
-    setModalType("reserva-realizada");
-  };
-
-
+  const [ufcgParticipants, setUfcgParticipants] = useState([]);
+  const [externalParticipants, setExternalParticipants] = useState([]);
+  const [users, setUsers] = useState([]); // Lista de usuários cadastrados
+  const [responsavel, setResponsavel] = useState(null); // Participante responsável
   const [selectedDate, setSelectedDate] = useState(null);
-  const [ufcgParticipants, setUfcgParticipants] = useState([
-    { name: "Fulano de Tal", email: "fulano.tal@curso.ufcg.edu.br" },
-    { name: "Ciclano Bla Bla Bla da Silva", email: "ciclano.silva@curso.ufcg.edu.br" },
-  ]);
-  const [externalParticipants, setExternalParticipants] = useState([
-    { name: "Fulano de Tal", email: "fulano.tal@example.com" },
-    { name: "Ciclano Bla Bla Bla da Silva", email: "ciclano.silva@example.com" },
-  ]);
-  const [responsavel, setResponsavel] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [arenas, setArenas] = useState([]);
+  const [arenaId, setArenaId] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const removeUfcgParticipant = (index) => {
-    setUfcgParticipants(ufcgParticipants.filter((_, i) => i !== index));
+  // Função para buscar arenas disponíveis
+  const fetchArenas = async () => {
+    try {
+      const token = localStorage.getItem("access_token");
+      const response = await axios.get("http://127.0.0.1:8000/v1/arenas/", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (Array.isArray(response.data.data)) {
+        setArenas(response.data.data);
+      } else {
+        console.error("Resposta inesperada da API:", response.data);
+        setErrorMessage("Erro ao carregar arenas. Resposta inesperada do servidor.");
+      }
+    } catch (error) {
+      console.error("Erro ao buscar arenas:", error);
+      setErrorMessage("Erro ao carregar arenas. Tente novamente mais tarde.");
+    }
   };
 
-  const removeExternalParticipant = (index) => {
-    setExternalParticipants(externalParticipants.filter((_, i) => i !== index));
+  // Função para buscar usuários cadastrados
+  const fetchUsers = async () => {
+    try {
+      const token = localStorage.getItem("access_token");
+      const response = await axios.get("http://127.0.0.1:8000/v1/users/", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (Array.isArray(response.data)) {
+        setUsers(response.data); // Atualiza a lista de usuários cadastrados
+      } else {
+        console.error("Resposta inesperada da API:", response.data);
+        setErrorMessage("Erro ao carregar usuários. Resposta inesperada do servidor.");
+      }
+    } catch (error) {
+      console.error("Erro ao buscar usuários:", error);
+      setErrorMessage("Erro ao carregar usuários. Tente novamente mais tarde.");
+    }
   };
 
-  const addUfcgParticipant = () => {
-    setUfcgParticipantInputs([...ufcgParticipantInputs, { email: "" }]);
+  // Função para adicionar participante da UFCG
+  const addUfcgParticipant = (index) => {
+    const email = ufcgParticipantInputs[index].email;
+    const user = users.find((u) => u.email === email && u.is_internal); // Verifica se o usuário é da UFCG
+    if (user) {
+      setUfcgParticipants([...ufcgParticipants, { name: user.full_name, email: user.email, id: user.id }]);
+      setUfcgParticipantInputs(ufcgParticipantInputs.filter((_, i) => i !== index));
+    } else {
+      setErrorMessage("Usuário não encontrado ou não é da UFCG.");
+    }
   };
-  
-  const addExternalParticipant = () => {
-    setExternalParticipantInputs([...externalParticipantInputs, { email: "" }]);
-  };
-  
 
-  const handleResponsavelChange = (participant, type) => {
-    setResponsavel({ participant, type });
+  // Função para adicionar participante externo
+  const addExternalParticipant = (index) => {
+    const email = externalParticipantInputs[index].email;
+    const user = users.find((u) => u.email === email && !u.is_internal); // Verifica se o usuário é externo
+    if (user) {
+      setExternalParticipants([...externalParticipants, { name: user.full_name, email: user.email, id: user.id }]);
+      setExternalParticipantInputs(externalParticipantInputs.filter((_, i) => i !== index));
+    } else {
+      setErrorMessage("Usuário não encontrado ou não é externo.");
+    }
   };
+
+  // Função para cadastrar reserva
+  const handleCadastrarReserva = async () => {
+    try {
+      const token = localStorage.getItem("access_token");
+
+      // Dados fixos para teste
+      const dadosParaEnvio = {
+        responsible_user_id: "8919a659-09ad-44a5-a40e-f2b1a1f6ff45",
+        arena_id: 6, 
+        start_date: "2025-04-02T18:00:00.000Z", 
+        end_date: "2025-04-02T19:00:00.000Z",
+        participants: [
+          "8919a659-09ad-44a5-a40e-f2b1a1f6ff45",
+        ],
+      };
+
+      console.log("Dados enviados para o backend:", dadosParaEnvio);
+
+      // Envia a requisição para o backend
+      const response = await axios.post("http://127.0.0.1:8000/v1/reservations/", dadosParaEnvio, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      console.log("Reserva realizada com sucesso:", response.data);
+      setModalType("reserva-realizada");
+      setIsModalOpen(true);
+    } catch (error) {
+      console.error("Erro ao cadastrar reserva:", error);
+
+      // Exibe o motivo do erro no console para depuração
+      if (error.response && error.response.data) {
+        console.error("Detalhes do erro:", error.response.data);
+        setErrorMessage(error.response.data.detail || "Erro ao cadastrar reserva. Verifique os dados e tente novamente.");
+      } else {
+        setErrorMessage("Erro ao cadastrar reserva. Tente novamente mais tarde.");
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchArenas();
+    fetchUsers(); // Busca os usuários cadastrados ao montar o componente
+  }, []);
 
   return (
     <div className="container-cadastrar-reserva">
@@ -84,13 +150,27 @@ const CadastrarReserva = () => {
         <div className="form-grid">
           <div className="input-group">
             <label htmlFor="esporte">Esporte</label>
-            <select id="esporte" className="select-input">
-              <option>Esporte - Local</option>
+            <select
+              id="esporte"
+              className="select-input"
+              value={arenaId || ""}
+              onChange={(e) => setArenaId(e.target.value)}
+            >
+              <option value="">Selecione uma arena</option>
+              {arenas.map((arena) => (
+                <option key={arena.id} value={arena.id}>
+                  {arena.name}
+                </option>
+              ))}
             </select>
           </div>
           <div className="input-group">
-            <label htmlFor="data-hora">DATA E HORA</label>
+            <label htmlFor="data-hora">DATA E HORA INÍCIO</label>
             <DateTimePicker selectedDate={selectedDate} onChange={setSelectedDate} />
+          </div>
+          <div className="input-group">
+            <label htmlFor="data-hora-fim">DATA E HORA FIM</label>
+            <DateTimePicker selectedDate={endDate} onChange={setEndDate} />
           </div>
         </div>
         <div className="participants-section">
@@ -98,7 +178,7 @@ const CadastrarReserva = () => {
           <div className="participant-group">
             <div className="title-and-button">
               <h4 className="sub-title">UFCG</h4>
-              <div className="add-button" onClick={addUfcgParticipant}>
+              <div className="add-button" onClick={() => setUfcgParticipantInputs([...ufcgParticipantInputs, { email: "" }])}>
                 <ion-icon name="add-circle-outline" className="add-button"></ion-icon>
               </div>
             </div>
@@ -115,45 +195,36 @@ const CadastrarReserva = () => {
                   }}
                 />
                 <div className="action-buttons">
-                  <ConfirmIcon
-                    onClick={() => {
-                      setUfcgParticipants([...ufcgParticipants, participant]);
-                      setUfcgParticipantInputs(ufcgParticipantInputs.filter((_, i) => i !== index));
-                    }}
-                  />
+                  <ConfirmIcon onClick={() => addUfcgParticipant(index)} />
                   <CancelIcon
-                    onClick={() => {
-                      setUfcgParticipantInputs(ufcgParticipantInputs.filter((_, i) => i !== index));
-                    }}
+                    onClick={() => setUfcgParticipantInputs(ufcgParticipantInputs.filter((_, i) => i !== index))}
                   />
                 </div>
               </div>
             ))}
             <div className="participant-list">
               {ufcgParticipants.map((participant, index) => (
-                <React.Fragment key={index}>
-                  <div className="participant-item">
-                    <span>{participant.name || "Novo Participante"}</span>
-                    <span className="participant-email">{participant.email}</span>
-                    <div className="action-icons">
-                      <input
-                        type="checkbox"
-                        checked={responsavel?.participant === participant && responsavel?.type === 'ufcg'}
-                        onChange={() => handleResponsavelChange(participant, 'ufcg')}
-                      />
-                      <label className="responsavel-label">Marcar como responsável</label>
-                      <TrashIcon onClick={() => removeUfcgParticipant(index)} className="trash-icon" />
-                    </div>
+                <div key={index} className="participant-item">
+                  <span>{participant.name}</span>
+                  <span className="participant-email">{participant.email}</span>
+                  <div className="action-icons">
+                    <input
+                      type="radio"
+                      name="responsavel"
+                      checked={responsavel?.id === participant.id}
+                      onChange={() => setResponsavel(participant)}
+                    />
+                    <label className="responsavel-label">Responsável</label>
+                    <TrashIcon onClick={() => setUfcgParticipants(ufcgParticipants.filter((_, i) => i !== index))} />
                   </div>
-                </React.Fragment>
+                </div>
               ))}
             </div>
-            <div className="separator-line"></div>
           </div>
           <div className="participant-group">
             <div className="title-and-button">
               <h4 className="sub-title">USUÁRIOS EXTERNOS</h4>
-              <div className="add-button" onClick={addExternalParticipant}>
+              <div className="add-button" onClick={() => setExternalParticipantInputs([...externalParticipantInputs, { email: "" }])}>
                 <ion-icon name="add-circle-outline" className="add-button"></ion-icon>
               </div>
             </div>
@@ -170,105 +241,38 @@ const CadastrarReserva = () => {
                   }}
                 />
                 <div className="action-buttons">
-                  <ConfirmIcon
-                    onClick={() => {
-                      setExternalParticipants([...externalParticipants, participant]);
-                      setExternalParticipantInputs(externalParticipantInputs.filter((_, i) => i !== index));
-                    }}
-                  />
+                  <ConfirmIcon onClick={() => addExternalParticipant(index)} />
                   <CancelIcon
-                    onClick={() => {
-                      setExternalParticipantInputs(externalParticipantInputs.filter((_, i) => i !== index));
-                    }}
+                    onClick={() => setExternalParticipantInputs(externalParticipantInputs.filter((_, i) => i !== index))}
                   />
                 </div>
               </div>
             ))}
             <div className="participant-list">
               {externalParticipants.map((participant, index) => (
-                <React.Fragment key={index}>
-                  <div className="participant-item">
-                    <span>{participant.name || "Novo Participante"}</span>
-                    <span className="participant-email">{participant.email}</span>
-                    <div className="action-icons">
-                      <input
-                        type="checkbox"
-                        checked={responsavel?.participant === participant && responsavel?.type === 'external'}
-                        onChange={() => handleResponsavelChange(participant, 'external')}
-                      />
-                      <label className="responsavel-label">Marcar como responsável</label>
-                      <TrashIcon onClick={() => removeExternalParticipant(index)} className="trash-icon" />
-                    </div>
+                <div key={index} className="participant-item">
+                  <span>{participant.name}</span>
+                  <span className="participant-email">{participant.email}</span>
+                  <div className="action-icons">
+                    <input
+                      type="radio"
+                      name="responsavel"
+                      checked={responsavel?.id === participant.id}
+                      onChange={() => setResponsavel(participant)}
+                    />
+                    <label className="responsavel-label">Responsável</label>
+                    <TrashIcon onClick={() => setExternalParticipants(externalParticipants.filter((_, i) => i !== index))} />
                   </div>
-                </React.Fragment>
+                </div>
               ))}
             </div>
-            <div className="separator-line"></div>
           </div>
         </div>
         <div className="submit-button-container">
-          <button className="submit-button" onClick={handleCadastrarClick}>Cadastrar Reserva</button>
+          <button className="submit-button" onClick={handleCadastrarReserva}>
+            Cadastrar Reserva
+          </button>
         </div>
-
-        {/* Modal de sobrescrever Horário */}
-        {isModalOpen && modalType === "sobrescrever-horario" && (
-          <ModalTwoOptions
-            iconName="triangulo-amarelo"
-            modalText="Já existe uma reserva agendada para este horário. Deseja sobrescrever?"
-            buttonTextOne="Sim"
-            buttonColorOne="red"
-            onClickButtonOne={handleCadastrarReserva}
-
-            buttonTextTwo="Não"
-            onClickButtonTwo={() => setIsModalOpen(false)}
-          />
-        )}
-
-        {/* Modal de reserva realizada */}
-        {isModalOpen && modalType === "reserva-realizada" && (
-          <ModalOneOption
-            iconName="calendario-check"
-            modalText="Reserva realizada com sucesso"
-            buttonText="Voltar"
-            buttonPath={"/visualizar-reservas"}
-          />
-        )}
-
-        {/* Modal de Confirmação do cadastro */}
-        {isModalOpen && modalType === "confirmar-cadastro" && (
-          <ModalTwoOptions
-            iconName="calendario-relogio"
-            modalText="Deseja confirmar sua reserva?"
-            buttonTextOne="Continuar editando"
-            onClickButtonOne={() => setIsModalOpen(false)}
-            buttonTextTwo="Confirmar"
-            onClickButtonTwo={handleCadastrarReserva}
-          />
-        )}
-
-        {/* Modal de horário indisponivel */}
-        {isModalOpen && modalType === "horario-indisponivel" && (
-          <ModalOneOption
-            iconName="calendario-erro"
-            modalText="Data/horário escolhido indisponível. Tente novamente"
-            buttonText="Tentar novamente"
-            onClick={() => setIsModalOpen(false)}
-          />
-        )}
-
-        {/* Modal pra caso o local esteja indisponivel ou o usuario responsavel nao seja permitido 
-             ou haja qualquer outro problema com os dados inseridos*/}
-        {isModalOpen && modalType === "credenciais-invalidas" && (
-          <ModalOneOption
-            iconName="circulo-erro"
-            modalText="Credenciais inválidas!
-                              Cheque os dados inseridos e
-                              tente novamente"
-            buttonText="Tentar novamente"
-            onClick={() => setIsModalOpen(false)}
-          />
-        )}
-
       </section>
     </div>
   );
